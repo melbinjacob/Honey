4aa9949c53a6e9b54cdb3f7fa4bbc72d7dc61080
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-10-26 14:54:52 +0530

    Reduced designations

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Designer", "Artist", "Painter", "Teacher", "Artist"],
+        strings: ["Designer","Artist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
